7fc485b907c14a77119e2c767845de5871dcab51
Andrew Potter <hello@drewpotter.com>
2019-11-28 13:28:16 +0000

    Refactor code as suggested

--- species.rb
+++ species.rb
@@ -1,12 +1,12 @@
 require 'byebug'
 
 module Species
-extend self
+  extend self
 
   def order_species species_data
     # Critically Endangered, Endangered, Vulnerable
     # CR is the most endangered, then comes EN, third VU
     # Fortunately these are alphabetically ordered.s
-    species_data.sort_by! { |scientific_name, common_name, redlist_status, iucn_redlist_url| redlist_status }
+    species_data.sort_by! { |hash| hash['redlist_status'] }
   end
 end
