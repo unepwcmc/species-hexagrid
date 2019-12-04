acf62cf6dace8ee1751b78e62c7218e60d5db6ee
Andrew Potter <hello@drewpotter.com>
2019-12-04 17:13:39 +0000

    Initial work towards s3 select for querying the csv data inside a bucket

--- app.rb
+++ app.rb
@@ -13,12 +13,38 @@ end
 get '/species/:id' do
   require 'csv'
   require 'aws-sdk-resources'
+  require 'byebug' # TODO delete this
   require_relative 's3'
   require_relative 'file_manager'
   require_relative 'species'
+  require 'aws-sdk-s3'
 
   filename = "test_#{params[:id]}.csv"
   @rows = []
+
+  client = Aws::S3::Client.new(region: ENV['AWS_REGION'])
+
+  params = {
+    bucket: ENV['AWS_BUCKET'],
+    key: 'test_3.csv',
+    expression_type: 'SQL',
+    expression: "SELECT * FROM S3Object WHERE redlist_status = 'CR'",
+    input_serialization: {
+      csv: { file_header_info: 'USE'}
+    },
+    output_serialization: {
+      csv: {}
+    }
+  }
+
+  client.select_object_content(params) do |stream|
+
+    # Callback for every event that arrives
+    stream.on_event do |event|
+       puts event.event_type
+       puts event.payload.read unless event.nil?
+    end
+  end
 
   FileManager.with_file(filename) do
     S3.new.get_file(filename)
