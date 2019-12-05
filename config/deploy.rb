1ac5918cbe9f3a00229d861046f53852a560de9e
Stuart Watson <stuart.watson@unep-wcmc.org>
2019-12-05 16:17:58 +0000

    Added staging deploy

---
+++ config/deploy.rb
@@ -0,0 +1,58 @@
+# config valid only for current version of Capistrano
+lock "~> 3.11.2"
+
+set :application, 'species-hexagrid'
+
+set :repo_url, 'git@github.com:unepwcmc/species-hexagrid.git'
+
+
+set :deploy_user, 'wcmc'
+
+
+set :backup_path, "/home/#{fetch(:deploy_user)}/Backup"
+
+
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
+
+# Default value for :scm is :git
+set :scm, :git
+set :scm_username, "unepwcmc-read"
+
+
+set :rvm_type, :user
+set :rvm_ruby_version, '2.6.1'
+
+
+
+set :ssh_options, {
+  forward_agent: true,
+}
+
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+#set :log_level, :debug
+
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+#set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+set :linked_files, %w{config/database.yml .env} 
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+set :passenger_restart_with_touch, false
