52612616887e9b490de864d69868cc9b547b1381
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-12-19 17:49:51 +0000

    Fetch and show also NT and DD species

--- species.rb
+++ species.rb
@@ -3,10 +3,17 @@ module Species
 
   #KEYS = ['scientific_name','common_name','redlist_status','iucn_redlist_url'].freeze
   KEYS = %w(ID_NO HEX_ID binomial common_nam category).freeze
+  IUCN_CATEGORIES = {
+    'CR' => 'Critically Endangered',
+    'EN' => 'Endangered',
+    'VU' => 'Vulnerable',
+    'NT' => 'Near Threatened',
+    'DD' => 'Data Deficient'
+  }.freeze
   THREATENED = %w(CR EN VU).freeze
 
-  def threatened_list
-    THREATENED.map { |c| "'#{c}'" }.join(',')
+  def categories_list
+    IUCN_CATEGORIES.keys.map { |c| "'#{c}'" }.join(',')
   end
 
   def keys
